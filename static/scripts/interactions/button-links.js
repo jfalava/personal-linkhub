52bd29519a7df3b84744626e8451c9b6120674a1
Jorge Fernando Álava <contacto@jfalava.eu>
2023-09-01 08:52:35 +0200

    v0.3c

--- static/scripts/interactions/button-links.js
+++ static/scripts/interactions/button-links.js
@@ -8,7 +8,7 @@
     window.open('https://www.linkedin.com/in/jfalava/', '_blank');
   };
   document.getElementById('am-button').onclick = function() {
-    window.open('https://music.apple.com/profile/jfalava', '_blank');
+    window.open('https://music.apple.com/es/playlist/20tracks/pl.u-9N9LL2eI2K5oXV', '_blank');
   };
     document.getElementById('keybase-button').onclick = function() {
     window.open('https://keybase.io/jfalava', '_blank');
